d0f4e48b5cd17a720b8c37a0c2a626f2f5f572c1
RodionPlakhotniuk <raidertwk@gmail.com>
2026-03-19 20:27:21 +0100

    fix: apply mentor feedback

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+
+    private final Random random = new Random();
+
     public Ball getRandomBall() {
-        ColorSupplier colorSupplier = new ColorSupplier();
         String randomColor = colorSupplier.getRandomColor();
-        Random random = new Random();
-        int randomNumber = random.nextInt(100);
+        int randomNumber = random.nextInt(101);
 
         Ball ball = new Ball(randomColor, randomNumber);
 
